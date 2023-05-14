022a9a8dbae98ad2606947e60847be61b28ee185
Jakub <jakub.fita3112@gmail.com>
2023-05-14 18:12:20 +0200

    feature: new sounds

--- bomb.py
+++ bomb.py
@@ -2,4 +2,4 @@ from game_object import GameObject
 
 class Bomb(GameObject):
     def __init__(self, window_size):
-        super().__init__(window_size=window_size, texture="images/Poisonous_Potato.webp")
+        super().__init__(window_size=window_size, texture="images/bomb.webp")
